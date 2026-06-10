09358e978716e8463f906fae861991fc5df350c2
Garima Mittal <garima14022004@gmail.com>
2026-06-10 14:54:45 +0530

    Add anagram checking method in Solution class
    Implement a method to check if two strings are anagrams by comparing character frequencies.

---
+++ 242. Valid Anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int freq[] = new int[26];
+        
+        if (s.length() != t.length()){
+            return false;
+        }
+        for (int i = 0 ; i < s.length() ; i++){
+            int val = s.charAt(i) - 'a' ;
+            freq[val]++;
+        }
+
+        for (int i = 0 ; i < t.length() ; i++){
+            int val = t.charAt(i) - 'a';
+            freq[val]--;
+        }
+
+        for (int i = 0 ; i < 26 ; i++){
+            if (freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
