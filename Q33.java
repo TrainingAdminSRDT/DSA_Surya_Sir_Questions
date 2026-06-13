fb45fd2d85cbb92dd62d3b239a0a9021bd301725
sunny74434 <sunnysahu74434@gmail.com>
2026-06-13 13:35:16 +0530

    Add files via upload

--- Q33.java
+++ Q33.java
@@ -1,33 +1,40 @@
+
+
 public class Q33 {
+  
+
     public int search(int[] nums, int target) {
 
-        int low = 0;
-        int high = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
 
-        while(low <= high) {
+        while(left <= right) {
 
-            int mid = low + (high - low) / 2;
+            int mid = left + (right - left) / 2;
 
             if(nums[mid] == target) {
                 return mid;
             }
-            if(nums[low] <= nums[mid]) {
 
-                if(target >= nums[low] && target < nums[mid]) {
-                    high = mid - 1;
-                } 
+            // left half sorted
+            if(nums[left] <= nums[mid]) {
+
+                if(target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                }
                 else {
-                    low = mid + 1;
+                    left = mid + 1;
                 }
             }
 
+            // right half sorted
             else {
 
-                if(target > nums[mid] && target <= nums[high]) {
-                    low = mid + 1;
+                if(target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
                 }
                 else {
-                    high = mid - 1;
+                    right = mid - 1;
                 }
             }
         }
