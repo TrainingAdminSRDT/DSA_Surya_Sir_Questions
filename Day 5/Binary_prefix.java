8b060e0b205e73f32ddf0125ed3f87c7ce0774d6
dipanshi2309 <dipanshisrivastava10@gmail.com>
2026-06-13 11:19:15 +0530

    Add files via upload

---
+++ Day 5/Binary_prefix.java
@@ -0,0 +1,14 @@
+import java.util.List;
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+         List<Boolean> ans = new ArrayList<>();
+        int rem = 0;
+
+        for (int bit : nums) {
+            rem = (rem * 2 + bit) % 5;
+            ans.add(rem == 0);
+        }
+
+        return ans;
+    }
+}
