908ff8018f48932ae6f26ae802355ac7f3670ba6
0705rishi <0705.rishi@gmail.com>
2026-06-13 10:10:44 +0530

    Add files via upload

--- reverseString.java
+++ reverseString.java
@@ -1,15 +1,13 @@
+package Day5;
+
 class Solution {
     public void reverseString(char[] s) {
-
-        int start = 0;
-        int end = s.length - 1;
-
-        while(start <= end){
-
+        int start=0;
+        int end= s.length-1;
+        while(start<=end){
             char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
-
+            s[start]= s[end];
+            s[end]=temp;
             start++;
             end--;
         }
