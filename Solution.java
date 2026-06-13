0dd49bff87d858f8cae262ece804ae37234e25a6
riyamani7 <maniriyaa.7@gmail.com>
2026-06-13 10:00:41 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,13 +1,13 @@
 public class Solution {
-    public int addDigits(int num) {
-        while (num >= 10) {
-            int sum = 0;
-            while (num > 0) {
-                sum = sum + (num % 10);
-                num = num / 10;
-            }
-            num = sum;
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+
+        while (p1 != p2) {
+            p1 = (p1 == null) ? headB : p1.next;
+            p2 = (p2 == null) ? headA : p2.next;
         }
-        return num;
+
+        return p1; // Intersection node or null
     }
 }
