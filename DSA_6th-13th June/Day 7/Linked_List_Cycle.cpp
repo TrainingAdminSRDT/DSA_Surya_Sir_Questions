e509101eb08e203ba2d6d43666dd7cec3f94634c
viraatgarg03 <viraatgarg3@gmail.com>
2026-06-13 11:59:57 +0530

    Add files via upload

---
+++ DSA_6th-13th June/Day 7/Linked_List_Cycle.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *a=head;
+        ListNode *b=head;
+        while ( a && a->next)
+        {
+            a=a->next->next;
+            b=b->next;
+            if(a==b)
+            { 
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
