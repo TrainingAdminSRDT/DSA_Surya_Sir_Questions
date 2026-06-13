0e8331d36c09f49c5d68211c0ddd1050ed6dc418
Aditya Asthana <adityaasthana1807@gmail.com>
2026-06-13 09:21:18 +0530

    Create binarysearch.java

---
+++ binarysearch.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=0;
+        int r=nums.length-1;
+        int mid=(l+r)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        while(l<r){
+            if(target<nums[mid]){
+                r=mid-1;
+                mid=(l+r)/2;
+            }
+            if(target>nums[mid]){
+                l=mid+1;
+                mid=(l+r)/2;
+            }
+            if(nums[mid]==target){
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
