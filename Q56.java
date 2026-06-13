b39bffafceec135e44a8dfaff1d6d3ddd1be9fcc
sunny74434 <sunnysahu74434@gmail.com>
2026-06-13 10:35:08 +0530

    Add files via upload

---
+++ Q56.java
@@ -0,0 +1,28 @@
+
+
+public class Q56 {
+
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b) -> a[0]-b[0]);
+        List<int[]> ans = new ArrayList<>();
+        int[] current = intervals[0];
+        ans.add(current);
+
+        for(int i=1;i< intervals.length;i++){
+            int[] next = intervals[i];
+
+            if(next[0] <= current[1]){
+                current[1] = Math.max(current[1],next[1]);
+            }
+            else{
+                current = next;
+                ans.add(current);
+            }
+        }
+        return ans.toArray(new int[ans.size()][]);
+
+        
+    }
+}
+    
+
