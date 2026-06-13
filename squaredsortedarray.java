d96d4feed86b1b6dd961cc1ff4482c87a1b6bcba
Aditya Asthana <adityaasthana1807@gmail.com>
2026-06-13 09:20:35 +0530

    Create squaredsortedarray.java

---
+++ squaredsortedarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n=nums.length;
+        int[] ans=new int[n];
+        int left=0; 
+        int right=n-1;
+        int pos=n-1;
+        while(left<=right){
+            int leftsq=nums[left]*nums[left];
+            int rightsq=nums[right]*nums[right];
+            if(leftsq>rightsq){
+                ans[pos]=leftsq;
+                left++;
+            }
+            else{
+                ans[pos]=rightsq;
+                right--;
+            }
+            pos--;
+        }
+        return ans;
+    }
+}
